af16fb3354e001ec8ed2129f6982f9ccdc419b38
Fawekun Temidayo <125440730+Whyrex007@users.noreply.github.com>
2023-10-04 15:56:39 +0100

    Create Variables | The_Geometrizer.js

---
+++ Variables | The_Geometrizer.js
@@ -0,0 +1,6 @@
+let Radius = 50;
+const Area = 3.142 * Radius * Radius;
+const Circumference = Math.floor(2 * 3.142 * Radius);
+
+console.log(`The circumference is ${Circumference} meters`)
+console.log(`The area is ${Area} meter square`)
