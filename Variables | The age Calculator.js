a96f8f13c29169d8c7f149af111e5937a8a56ab7
Fawekun Temidayo <125440730+Whyrex007@users.noreply.github.com>
2023-10-04 15:56:08 +0100

    Create Variables | The age Calculator.js

---
+++ Variables | The age Calculator.js
@@ -0,0 +1,6 @@
+const Birth_year = 1994;
+const Future_year = 2050;
+let possible_age1 = Future_year - Birth_year;
+let possible_age2 = (Future_year - Birth_year) - 1;
+
+console.log(`I will be ${possible_age2} or ${possible_age1} in ${Future_year}`)
