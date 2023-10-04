50f47b63710cd8433f7fae7e3bd030e3afb628d4
Fawekun Temidayo <125440730+Whyrex007@users.noreply.github.com>
2023-10-04 15:57:31 +0100

    Create Functions | The Geometrizer.js

---
+++ Functions | The Geometrizer.js
@@ -0,0 +1,12 @@
+function calCircumference(Radius){
+  const Circumference = Math.floor(2 * 3.142 * Radius)
+  console.log(`The circumference is ${Circumference} meters`)
+}
+calCircumference(45);
+
+
+function calArea(Radius){
+  const Area = Math.floor(3.142 * Radius * Radius);
+  console.log(`The area is ${Area} meter square`)
+}
+calArea(15);
