38d03bdd77de53f3c56edef3df528ca36f636cef
Fawekun Temidayo <125440730+Whyrex007@users.noreply.github.com>
2023-10-04 15:58:01 +0100

    Create Functions | The Puppy Age Calculator.js

---
+++ Functions | The Puppy Age Calculator.js
@@ -0,0 +1,8 @@
+function calculateDogAge(puppyAge,HumantoDog){
+  let x = 7 * puppyAge;
+  let y = HumantoDog/7;
+  console.log(`Your doggie is ${x} years old in dog years!`)
+   console.log(`Your doggie is ${y} years old in human years!`)
+}
+
+calculateDogAge()
